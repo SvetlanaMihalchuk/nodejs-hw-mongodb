00a3009d3a8a99819570f554d1b27f1ae1aa4822
Svetlana <mihalchuk82@gmail.com>
2025-04-08 22:05:46 +0300

    New HW2

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,4 +3,4 @@ import ContactCollection from '../db/models/contacts.js';
 export const getContacts = () => ContactCollection.find();
 
 export const getContactById = (contactId) =>
-  ContactCollection.findOneAndDelete({ _id: contactId });
+  ContactCollection.findOne({ _id: contactId });
